2d41e1829ad0926d45607c4865b0bda62e8646ed
Erick Mori <erick.mori21@gmail.com>
2020-09-20 16:04:46 -0300

    Troquei o arquivo routes por rooutes.tsx

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,12 +3,11 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Routes from './routes';
+import Routes from './Rooutes'
 
 function App() {
   return (
     <Routes />
-
   );
 }
 
